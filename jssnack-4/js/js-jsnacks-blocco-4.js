c4309af6d348623ef4f73007eac9ba36fa9b120d
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-11 19:57:52 +0100

    feat: add a list array of ten numbers

--- jssnack-4/js/js-jsnacks-blocco-4.js
+++ jssnack-4/js/js-jsnacks-blocco-4.js
@@ -2,7 +2,6 @@
 
 const listNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] // Creiamo la lista di numeri da 1 a 10
 
-
 console.log(listNumber);
 
 let sum = 0
---
+++ jssnack-5/js/js-jsnacks-blocco-5.js
@@ -0,0 +1,11 @@
+// Dato un array contenente 10 numeri, stampare in console il numero più grande.
+
+// const listRandomNumber = Math.max(4, 89, 43, 400, 7, 3, 76, 25, 8, 123); // Scriviamo una lista di 10 numeri
+// console.log(listRandomNumber);
+
+// Vedere qual è il più grandi di tutti
+// Stampare il numero più grande presente in lista
+
+const listRandomNumber = [4, 89, 43, 100, 7, 3, 76, 200, 8, 123];
+
+
