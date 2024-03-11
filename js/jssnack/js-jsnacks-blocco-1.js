9e85a142ed905dbad1c1443daf791c3166dad58d
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-11 14:05:38 +0100

    feat: add a parsint to the prompt to make sure that is a number

--- js/jssnack/js-jsnacks-blocco-1.js
+++ js/jssnack/js-jsnacks-blocco-1.js
@@ -3,7 +3,7 @@
 const numbers = []; // Creiamo la nostra lista vuota
 
 for (let i = 0; i < 5; i++) {
-    const messageInputNumber = prompt('Scrivi un numero');
+    const messageInputNumber = parseInt(prompt('Scrivi un numero'));
     console.log(messageInputNumber);
 }; // Chiediamo all'utente 5 numeri con un ciclo for
 
