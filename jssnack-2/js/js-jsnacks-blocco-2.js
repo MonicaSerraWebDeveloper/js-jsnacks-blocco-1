34eea423b88a05b48869f3e7424069eb50cc1cce
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-11 17:23:31 +0100

    feat: add all the comment to understand the process

---
+++ jssnack-2/js/js-jsnacks-blocco-2.js
@@ -0,0 +1,6 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+// Chiediamo all'utente di dirci un numero per 5 volte con un prompt
+// Facciamo la somma di tutti i numeri inseriti 
+// Stampiamo la somma finale 
