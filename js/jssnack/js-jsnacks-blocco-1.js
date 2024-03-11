a763e2e0efeb048944d2d616dfd8fcbf5bbaf709
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-11 14:01:05 +0100

    feat: add all the instruction as a comment

--- js/jssnack/js-jsnacks-blocco-1.js
+++ js/jssnack/js-jsnacks-blocco-1.js
@@ -1,2 +1,6 @@
 // Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
 
+const numbers = []; // Creiamo la nostra lista vuota
+// Chiediamo all'utente 5 numeri con un ciclo for
+// Inseriamo i 5 numeri nell'array vuoto
+// Verifichiamo che il numero scritto non sia già presente nell'array
