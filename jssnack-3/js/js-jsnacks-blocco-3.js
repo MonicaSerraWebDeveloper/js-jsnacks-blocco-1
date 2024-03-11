ef6a6a5120205e003b0c603deab6aeda9b7cd89f
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-11 18:03:45 +0100

    feat: add the condition that add only the odd number to our array list

--- jssnack-3/js/js-jsnacks-blocco-3.js
+++ jssnack-3/js/js-jsnacks-blocco-3.js
@@ -4,9 +4,14 @@
 
 const listNumber = [] // Creiamo una lista vuota pronta per essere riempita
 
-
 for (let i = 0; i < 6; i++) { 
     const userMessage = parseInt(prompt('Scrivi un numero'));
     console.log(userMessage); // Chiediamo 6 volte all'utente di inserire un numero
+
+    if (userMessage % 2 === 1) { 
+        listNumber.push(userMessage) // Scriviamo nella lista solo i numeri dispari che ci dice l'utente
+    }
 }
-// Scriviamo nella lista solo i numeri dispari che ci dice l'utente
+
+console.log(listNumber);
+
