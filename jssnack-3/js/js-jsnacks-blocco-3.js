d3e69fbc20d860bc13ee3841b8d73ae12bb37da7
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-11 17:54:03 +0100

    feat: add all the comments tu understand bettere the process

---
+++ jssnack-3/js/js-jsnacks-blocco-3.js
@@ -0,0 +1,7 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+
+// Creiamo una lista vuota pronta per essere riempita
+// Chiediamo 6 volte all'utente di inserire un numero
+// Scriviamo nella lista solo i numeri dispari che ci dice l'utente
