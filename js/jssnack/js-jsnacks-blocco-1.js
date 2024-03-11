9d37e7a5e5bdf7ba3037e55894962bf41f7d9a78
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-11 14:03:01 +0100

    feat: created the array empty and a for cicle to ask 5 times a number to the user

--- js/jssnack/js-jsnacks-blocco-1.js
+++ js/jssnack/js-jsnacks-blocco-1.js
@@ -1,6 +1,11 @@
 // Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
 
 const numbers = []; // Creiamo la nostra lista vuota
-// Chiediamo all'utente 5 numeri con un ciclo for
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}; // Chiediamo all'utente 5 numeri con un ciclo for
+
+
 // Inseriamo i 5 numeri nell'array vuoto
 // Verifichiamo che il numero scritto non sia già presente nell'array
