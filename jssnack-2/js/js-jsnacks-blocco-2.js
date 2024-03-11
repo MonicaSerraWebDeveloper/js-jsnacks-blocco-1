04d082c8389f9536941382a6ebf251de2e2c0484
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-11 17:38:15 +0100

    feat: add the cicle for to show the promt 5 times to the user

--- jssnack-2/js/js-jsnacks-blocco-2.js
+++ jssnack-2/js/js-jsnacks-blocco-2.js
@@ -2,5 +2,12 @@
 // Il programma stampa la somma di tutti i numeri inseriti.
 
 // Chiediamo all'utente di dirci un numero per 5 volte con un prompt
+
+let sum = 0;
+for (let i = 0; i < 5; i++) {
+    const userMessage = parseInt(prompt('Scrivi un numero'));
+    console.log(userMessage);
+}
+
 // Facciamo la somma di tutti i numeri inseriti 
 // Stampiamo la somma finale 
