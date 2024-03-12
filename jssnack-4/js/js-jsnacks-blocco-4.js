58bda398105414d662cae654fdc69d22c43cd186
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-12 10:25:09 +0100

    fix: changed in snack 4 the array in a variable

--- jssnack-4/js/js-jsnacks-blocco-4.js
+++ jssnack-4/js/js-jsnacks-blocco-4.js
@@ -1,17 +1,17 @@
 // Calcola la somma e la media dei primi 10 numeri.
 
-const listNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] // Creiamo la lista di numeri da 1 a 10
+const listNumber = 10; // Creiamo la lista di numeri da 1 a 10
 
 console.log(listNumber);
 
 let sum = 0
-for (let i = 0; i < listNumber.length; i++) {
-    sum = sum + listNumber[i] // Facciamo la somma di ogni numero 
+for (let i = 0; i <= listNumber; i++) {
+    sum = sum + i // Facciamo la somma di ogni numero 
 }
 
 console.log(sum);
 
-const takeTheAverage = sum / listNumber.length // Dividiamo per il numero di cifre 
+const takeTheAverage = sum / listNumber // Dividiamo per il numero di cifre 
 
 console.log(takeTheAverage); // Stampare il numero finale che è la media 
 
